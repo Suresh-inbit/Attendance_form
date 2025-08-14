423308aae5123197a3556ba49cfa993b494fdffb
Suresh-inbit <suresh.inbit@gmail.com>
2025-08-14 15:57:41 +0530

    hash route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import StudentFormPage from './pages/StudentFormPage';
 import AttendanceListPage from './pages/AttendanceListPage';
 import AllRecordsPage from './pages/AllRecordsPage';
