ded47b37edc51dc973e894d39053fffd75d44339
Suresh-inbit <suresh.inbit@gmail.com>
2025-08-23 19:01:40 +0530

    port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: 'attendance-form.onrender.com'
   },
   build: {
     sourcemap: false, // disable source maps entirely in build
