3a4476e8cf4b2a8bad884e7f1f092664504b72d2
Suresh-inbit <suresh.inbit@gmail.com>
2025-08-23 19:36:19 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch((err) => {
     console.error('❌ MongoDB connection error:', err.message);
   });
-
+  
   app.listen(PORT, () => {
     const interfaces = os.networkInterfaces();
     let ipAddresses = [];
